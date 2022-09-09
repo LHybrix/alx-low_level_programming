d5dee3a80be1d5df9722764e9ffeda95fbb0fd99
LHybrix <lord.kidou@gmail.com>
2022-09-08 18:46:33 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * Main - Entry
+ * Description: Advanced
+ * Return: 0
+ */
+
+int main(void)
+{
+	cout << "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 1>&2;
+	return(0);
+}
