76998900d006ae7ed28ca795369675f0874c6928
LHybrix <lord.kidou@gmail.com>
2022-09-13 05:19:11 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: "IDK"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	return (0);
+}
