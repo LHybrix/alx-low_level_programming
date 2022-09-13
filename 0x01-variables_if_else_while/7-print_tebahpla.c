27c63fb9f48d4af3e18c5ea6bc3db0dfe68fbcd0
LHybrix <lord.kidou@gmail.com>
2022-09-13 07:06:23 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: "IDK"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
