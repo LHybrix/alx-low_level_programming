38dfcea12b1760918c34f067c7be8fded9cb26cc
LHybrix <lord.kidou@gmail.com>
2022-09-13 06:54:38 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * Description: "IDK"
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
