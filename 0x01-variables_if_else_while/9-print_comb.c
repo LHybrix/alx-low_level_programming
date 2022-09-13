bbef8805e76ac32bad9cfec8c98644bd10d27896
LHybrix <lord.kidou@gmail.com>
2022-09-13 07:53:20 -0700

    9 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '8'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
+		if (i <= '8')
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 	return (0);
 }
