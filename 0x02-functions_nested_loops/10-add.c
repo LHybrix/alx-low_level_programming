2c95ea5ea89891e58b7726551f340bd1a34e7bf6
LHybrix <lord.kidou@gmail.com>
2022-09-14 15:57:15 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - myfunc
+ * @num1: 1st parameter
+ * @num2: 2nd parameter
+ * Description: Sum of 2 arguments
+ * Return: result
+ */
+
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
