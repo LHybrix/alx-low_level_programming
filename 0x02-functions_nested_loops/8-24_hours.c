a8dc7b10a3c876b03aea8a3d7ac68fd5c02c9fd3
LHybrix <lord.kidou@gmail.com>
2022-09-14 12:03:37 -0700

    8 again

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,21 +8,26 @@
 
 void jack_bauer(void)
 {
-	for (int i = 0; i <= 2; i++)
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i <= 2; i++)
 	{
 		_putchar(i + '0');
 		if (i != 2)
 		{
-			for (int j = 0; j <= 9; j++)
+			for (j = 0; j <= 9; j++)
 			{
 				_putchar(j + '0');
 				_putchar(':');
-				for (int k = 0; k <= 6; k++)
+				for (k = 0; k <= 6; k++)
 				{
 					_putchar(k + '0');
 					if (k != 6)
 					{
-						for (int l = 0; l <= 9; l++)
+						for (l = 0; l <= 9; l++)
 						{
 							_putchar(l + '0');
 							_putchar('\n');
@@ -38,16 +43,16 @@ void jack_bauer(void)
 		}
 		else
 		{
-			for (int j = 0; j <= 3; j++)
+			for (j = 0; j <= 3; j++)
 			{
 				_putchar(j + '0');
 				_putchar(':');
-				for (int k = 0; k <= 6; k++)
+				for (k = 0; k <= 6; k++)
 				{
 					_putchar(k + '0');
 					if (k != 6)
 					{
-						for (int l = 0; l <= 9; l++)
+						for (l = 0; l <= 9; l++)
 						{
 							_putchar(l + '0');
 							_putchar('\n');
