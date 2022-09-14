11591cd15ba9db29c1b047cb347cdac6e31de00b
LHybrix <lord.kidou@gmail.com>
2022-09-14 05:57:18 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - myfunc
+ * @num: parameter
+ * Description: Absolute value
+ * Return: various
+ */
+
+int _abs(int num)
+{
+	if (num < 0)
+	{
+		return (num *= -1);
+	}
+	return (0);
+}
