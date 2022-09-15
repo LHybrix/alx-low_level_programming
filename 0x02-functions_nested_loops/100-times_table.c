812a60acb5f7b5f04e3844e6bc68965de0d32ca3
LHybrix <lord.kidou@gmail.com>
2022-09-14 19:31:38 -0700

    1adv 3ammiel

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,43 +9,43 @@
 
 void print_times_table(int n)
 {
-	if (!(n > 15) || !(n < 0))
-	{
-		for (int num = 0; num <= n; num++)
-		{
-			for (int num2 = 0; num2 <= n; num2++)
-			{
-				int r = num * num2;
+	int num, num2;
 
-				if (r < 10)
-				{
-					if (num2 != 0)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					_putchar(r + '0');
-				}
-				else if (r < 100)
+	if (n > 15 || n < 0)
+		return;
+	for (num = 0; num <= n; num++)
+	{
+		for (num2 = 0; num2 <= n; num2++)
+		{
+			int r = num * num2;
+			if (r < 10)
+			{
+				if (num2 != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(r / 10 + '0');
-					_putchar(r % 10 + '0');
-				}
-				else
-				{
-					_putchar(',');
 					_putchar(' ');
-					_putchar(r / 100 + '0');
-					_putchar((r / 10) % 10 + '0');
-					_putchar(r % 10 + '0');
 				}
+				_putchar(r + '0');
 			}
-			_putchar('\n');
+			else if (r < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(r / 100 + '0');
+				_putchar((r / 10) % 10 + '0');
+				_putchar(r % 10 + '0');
+			}
 		}
+		_putchar('\n');
 	}
 }
