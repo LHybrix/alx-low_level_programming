7e61a0a36fbcfb4ec82f7aa114a639fd195065d0
LHybrix <lord.kidou@gmail.com>
2022-09-15 17:35:56 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - myfunc
+ * @a: 1st parameter
+ * @b: 2nd parameter
+ * Description: product of 2 arguments
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
