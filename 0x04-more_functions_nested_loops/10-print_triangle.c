76b53e9dd37c3c245396b5df18fdc71def6c191d
LHybrix <lord.kidou@gmail.com>
2022-09-18 04:52:36 -0700

    10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,4 +9,23 @@
 
 void print_triangle(int size)
 {
+	int i;
+	int noOfSpaces;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (noOfSpaces = size - 1; noOfSpaces > 0; noOfSpaces--)
+			{
+				_putchar(' ');
+			}
+			_putchar('#');
+			_putchar('\n');
+		}
+	}
 }
