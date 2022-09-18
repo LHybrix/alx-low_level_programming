d3007f6747509e8c7ceed72f5156c805f3d33c45
LHybrix <lord.kidou@gmail.com>
2022-09-18 03:02:03 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,4 +9,22 @@
 
 void print_square(int size)
 {
+	int i;
+	int repeatWholeThing;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (repeatWholeThing = 0; repeatWholeThing < size; repeatWholeThing++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
