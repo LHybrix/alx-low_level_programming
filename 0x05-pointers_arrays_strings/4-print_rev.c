6ac5bb012e670e64b1588920f2d3babdc6d68443
LHybrix <lord.kidou@gmail.com>
2022-09-20 10:20:36 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_rev - myfunc
+  * @s: pointer parameter
+  * Description: Print but reversed
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int l = 0;
+	int *lp = &l;
+
+	for (; *s != '\0'; s++)
+	{
+		*lp = l + 1;
+	}
+	int lnIndex = *lp - 1;
+	int lnIndexP = *(s + lnIndex);
+
+	while (lnIndexP >= *s)
+	{
+		_putchar(lnIndexP);
+		lnIndexP--;
+	}
+}
